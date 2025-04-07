d9d89172ce3b554404634b6bd004ff5202b1c696
Soubhik Samanta <soubhiksamanta25@gmail.com>
2025-04-07 10:16:29 +0530

    Initial commit changes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express =require('express');
 const urlRoutes =require("./routes/url");
 const {connectToMongodb}=require("./connect");
 const {handelGenerateNewShortUR}=require("./controllers/url");
+const URL = require('./models/url'); 
 
 const app =express();
 const PORT =8001;
@@ -12,6 +13,27 @@ connectToMongodb('mongodb://localhost:27017/short-url')
 app.use(express.json());
 
 app.use("/url",urlRoutes)
+
+//shortid-redirect
+app.get("/:shortId", async (req, res) => {
+    const shortId = req.params.shortId;
+  
+    const entry = await URL.findOneAndUpdate(
+      {
+        shortId,
+      },
+      {
+        $push: {
+          visitHistory: {
+            timestamp: Date.now(),
+          },
+        },
+      }
+    );
+  
+    res.redirect(entry.redirectURL);
+  });
+  
 
 
 app.listen(PORT,()=>console.log(`server started at port :${PORT}`));
